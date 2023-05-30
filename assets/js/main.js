d94c4868e1d895d2925b19427c99bd89dc71c30f
Himanshu Raj <rajh91774@gmail.com>
2023-05-30 18:10:32 +0530

    added testimonial

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    responsiveClass: true,
+    margin:20,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1200: {
+            items: 2,
+            nav: false
+        }
+    }
+})
